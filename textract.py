e2194f6f91e57d82fdc9f6898280d71718e5c378
Santiago Gonzalez <santiagogo1993@gmail.com>
2025-04-10 09:35:35 -0500

    Add .gitignore Create fucntion using API textract from aws and add credentials

--- textract.py
+++ textract.py
@@ -1,6 +1,39 @@
+#! python
+import os
 import boto3
+import sys
+from dotenv import load_dotenv
 
-textract = boto3.client('textract')
+load_dotenv()
 
-with open('test1.jpg', 'rb') as img:
+
+textract = boto3.client(
+    'textract',
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCES_KEY"),
+    region_name=os.getenv("REGION_NAME")
+)
+
+#Load local document
+with open(sys.argv[1], 'rb') as img:
     document_bytes = img.read()
+
+response = textract.analyze_document(
+    Document={'Bytes': document_bytes},
+    FeatureTypes = ['FORMS', 'TABLES']
+)
+
+#Print text
+print("Extracted text:")
+for block in response['Blocks']:
+    if block['BlockType'] == 'LINE':
+        print(block['Text'])
+
+# print("\nKey-Value Paris:")
+# for block in response['Blocks']:
+#     if block['BlockType'] == 'KEY_VALUE_SET':
+#         print(f"{block.get('Text', 'N/A')}")
+
+# for block in response['Blocks']:
+#     if block['BlockType'] == 'TABLE':
+#         print(f"Table detected:e {block}")
